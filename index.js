b2a27b8cee496a09b5bddcc6eae3a869e3d318c4
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-04-22 17:33:48 +0200

    Added full board matrix in knightMoves() and the necessary test calls in the test() function

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function knightMoves() {}
+function knightMoves(start, end) {
+	let board = [
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[0, 1, 2, 3, 4, 5, 6, 7],
+	];
+}
 
-function test() {}
+function test() {
+	knightMoves([0, 0], [1, 2]);
+	knightMoves([0, 0], [3, 3]);
+	knightMoves([3, 3], [0, 0]);
+	knightMoves([0, 0], [7, 7]);
+	knightMoves([0, 0], [7, 7]);
+}
