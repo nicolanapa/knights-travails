f35ae7cb5b6083544cd34e98a265fbddbae97cd0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-04-25 15:13:32 +0200

    Trying to make a working algorithm which appends a random square in the knight List. Base for wrote, need to fix the if and appends

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import { Node, LinkedList } from "./nodeLinkedList.js";
+
+function getRandomMove(max = 3) {
+	return Math.floor(Math.random() * max);
+  }
+
 function knightMoves(start, end) {
 	// Data structure with all nodes linked?
 	// Use linked lists?
@@ -12,29 +18,49 @@ function knightMoves(start, end) {
 		[1, 0, 1, 0, 1, 0, 1, 0],
 	];
 
-	for (let i = 0; i < 1; i++) {
+	/*for (let i = 0; i < 1; i++) {
 		if (start[0] === end[0]) {
 			if (start[1] === end[1]) {
 				console.log("finished");
 			}
 		}
+	}*/
 
-		if (start[0] >= end[0]) {
-			console.log("first yes");
-		} else if (start[0] <= end[0]) {
-			console.log("first no");
-		}
+	let knight = new LinkedList();
+	knight.append(start);
 
-		if (start[1] >= end[1]) {
-			console.log("second no");
-		} else if (start[1] <= end[1]) {
-			console.log("second no");
+	/*if (start[0] === 0) {
+		knight.append([2, 1]);
+	} else if (start[0] > 0 && start[0] < 7) {
+	} else if (start[0] === 7) {
+	}*/
+
+	while(!knight.contains(end)) {
+		let randomValue1 = getRandomMove();
+		let randomValue2 = getRandomMove();
+
+		if (knight.tail()[0] === 0) {
+			
+			knight.append([getRandomMove(), getRandomMove()]);
+		} else if (knight.tail()[0] > 0 && knight.tail()[0] < 7) {
+			knight.append([getRandomMove(), getRandomMove()]);
+		} else if (knight.tail()[0] === 7) {
+			knight.append([knight.tail() - getRandomMove(), knight.tail() -  getRandomMove()]);
 		}
-		console.log(start, end);
-		console.log(start[0], end[0]);
-		console.log(start[1], end[1]);
 	}
+
+	console.log(knight.toString());
 }
+
+/*
+	Head of the list -> value: start
+
+	nextNode of the list -> any valid node
+	do this until the value is the same as end
+*/
+
+// 2 in any direction + 1 in left/right
+// 1 in any direction + 2 in left/right
 
 function test() {
 	knightMoves([0, 0], [1, 2]);
